a2f39078ad8046b3b3500e3df15170f736c90995
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2026-02-26 17:17:09 +0000

    feat: adding the create instance funcitionality with security group

---
+++ acs_assign1.py
@@ -0,0 +1,35 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+#retrieving the constants on .env
+KEY_NAME = os.environ['AWS_KEY_NAME']
+SECURITY_GROUP_ID = os.environ['AWS_SECURITY_GROUP_ID']
+
+ec2_resource = boto3.resource('ec2', region_name='us-east-1')
+
+
+new_instances = ec2_resource.create_instances(
+    ImageId='ami-04752fceda1274920',
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.nano',
+    KeyName= KEY_NAME,
+    SecurityGroupIds=[SECURITY_GROUP_ID],
+    Placement={
+        'AvailabilityZone': 'us-east-1b'},
+    TagSpecifications=[{
+        'ResourceType': 'instance',  
+        'Tags': [                     
+            {
+                'Key': 'Name',        
+                'Value': 'TestName' 
+            },
+            {
+                'Key': 'Test',
+                'Value': 'se apareceu, a tag ta funcionando'    
+            }
+        ]
+    }]
+    )
+
