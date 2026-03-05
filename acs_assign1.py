6d84d417db52689eb52ca7d842981a10e59c1082
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2026-03-05 18:17:58 +0000

    refactor: adding basic try catch sintax on the script

--- acs_assign1.py
+++ acs_assign1.py
@@ -46,61 +46,73 @@ s3_resource = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
 #create instance
-new_instances = ec2_resource.create_instances(
-    ImageId='ami-04752fceda1274920',
-    MinCount=1,
-    MaxCount=1,
-    InstanceType='t2.nano',
-    KeyName= KEY_NAME,
-    SecurityGroupIds=[SECURITY_GROUP_ID],
-    UserData=user_data_script,
-    Placement={
-        'AvailabilityZone': 'us-east-1b'},
-    TagSpecifications=[{
-        'ResourceType': 'instance',  
-        'Tags': [                     
-            {
-                'Key': 'Name',        
-                'Value': 'TestName' 
-            },
-            {
-                'Key': 'Test',
-                'Value': 'se apareceu, a tag ta funcionando'    
-            }
-        ]
-    }]
-    )
+try:
+    new_instances = ec2_resource.create_instances(
+        ImageId='ami-04752fceda1274920',
+        MinCount=1,
+        MaxCount=1,
+        InstanceType='t2.nano',
+        KeyName= KEY_NAME,
+        SecurityGroupIds=[SECURITY_GROUP_ID],
+        UserData=user_data_script,
+        Placement={
+            'AvailabilityZone': 'us-east-1b'},
+        TagSpecifications=[{
+            'ResourceType': 'instance',  
+            'Tags': [                     
+                {
+                    'Key': 'Name',        
+                    'Value': 'TestName' 
+                },
+                {
+                    'Key': 'Test',
+                    'Value': 'se apareceu, a tag ta funcionando'    
+                }
+            ]
+        }]
+        )
 
-instance = new_instances[0]
-instance.wait_until_running()
-instance.reload()
-print("instance is running")
+    instance = new_instances[0]
+    instance.wait_until_running()
+    instance.reload()
+    print("instance is running")
 
-#usign client to ensure that the status of the instance is ok
-#print(ec2_client.waiter_names) command that is used to see the list of avaible waiters names
-waiter = ec2_client.get_waiter('instance_status_ok')
-waiter.wait(InstanceIds=[instance.id])
-print(f"\nec2 website instance status ok")
+    #usign client to ensure that the status of the instance is ok
+    #print(ec2_client.waiter_names) command that is used to see the list of avaible waiters names
+    waiter = ec2_client.get_waiter('instance_status_ok')
+    waiter.wait(InstanceIds=[instance.id])
+    print(f"\nec2 website instance status ok")
+except Exception as error:
+    print(error)
+    exit(1)
 
 #creating AMI
-initials = "JV"
-timestamp = datetime.now().strftime("%Y-%m-%d%f") #datetime library was used to get the milesseconds
-ami_name = initials+"-"+timestamp
+try:
+    initials = "JV"
+    timestamp = datetime.now().strftime("%Y-%m-%d%f") #datetime library was used to get the milesseconds
+    ami_name = initials+"-"+timestamp
 
-image = instance.create_image(
-    Name=ami_name,
-    Description=f"AMI - timestamp: {timestamp}"
-)
+    image = instance.create_image(
+        Name=ami_name,
+        Description=f"AMI - timestamp: {timestamp}"
+    )
 
-print("Image created, waiting for it get available")
+    print("Image created, waiting for it get available")
 
-waiter_image = ec2_client.get_waiter('image_available')
-waiter_image.wait(ImageIds=[image.id])
-print(f"\nAMI created: {ami_name} ({image.id})")
+    waiter_image = ec2_client.get_waiter('image_available')
+    waiter_image.wait(ImageIds=[image.id])
+    print(f"\nAMI created: {ami_name} ({image.id})")
+except Exception as error:
+    print(error)
+    exit(1)
 
 #running the subprocess to download the image
+try:
+    subprocess.run(["curl","-O","https://setuacsresources.s3-eu-west-1.amazonaws.com/setulogo.jpeg"], check=True)
+except Exception as error:
+    print(error)
+    exit(1)
 
-subprocess.run(["curl","-O","https://setuacsresources.s3-eu-west-1.amazonaws.com/setulogo.jpeg"])
 #creating a bucket
 random_chars = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
 bucket_name = random_chars+"-jvsilva"  
@@ -113,59 +125,67 @@ except Exception as error:
  exit(1)
  
 #managing acess and files to be put on a bucket to be turn into an static website
-print("adding the image and the index.html to the bucket")
-s3_resource.Object(bucket_name, 'setulogo.jpeg').put(
-    Body=open('setulogo.jpeg', 'rb'), 
-    ContentType='image/jpeg'
-)
+try:
+    print("adding the image and the index.html to the bucket")
+    s3_resource.Object(bucket_name, 'setulogo.jpeg').put(
+        Body=open('setulogo.jpeg', 'rb'), 
+        ContentType='image/jpeg'
+    )
 
-s3_client.delete_public_access_block(Bucket=bucket_name)
+    s3_client.delete_public_access_block(Bucket=bucket_name)
 
-bucket_policy = {
-    "Version": "2012-10-17",
-    "Statement": [{
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": "s3:GetObject",
-        "Resource": f"arn:aws:s3:::{bucket_name}/*"
-    }]
-}
-s3_resource.Bucket(bucket_name).Policy().put(Policy=json.dumps(bucket_policy))
+    bucket_policy = {
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": f"arn:aws:s3:::{bucket_name}/*"
+        }]
+    }
+    s3_resource.Bucket(bucket_name).Policy().put(Policy=json.dumps(bucket_policy))
 
-html_content = f"""
-    <h1>João Victor Godoy da Silva</h1>
-    <img src="setulogo.jpeg">
-    <h2>Bucket: {bucket_name}</h2>
-"""
+    html_content = f"""
+        <h1>João Victor Godoy da Silva</h1>
+        <img src="setulogo.jpeg">
+        <h2>Bucket: {bucket_name}</h2>
+    """
 
-with open('index.html', 'w') as file:
-    file.write(html_content)
-    
-s3_resource.Object(bucket_name, 'index.html').put(
-    Body=open('index.html', 'rb'),
-    ContentType='text/html'
-)
+    with open('index.html', 'w') as file:
+        file.write(html_content)
+        
+    s3_resource.Object(bucket_name, 'index.html').put(
+        Body=open('index.html', 'rb'),
+        ContentType='text/html'
+    )
 
-website_configuration = {
-    'ErrorDocument': {'Key': 'error.html'},
-    'IndexDocument': {'Suffix': 'index.html'}
-}
+    website_configuration = {
+        'ErrorDocument': {'Key': 'error.html'},
+        'IndexDocument': {'Suffix': 'index.html'}
+    }
 
-bucket_website = s3_resource.BucketWebsite(bucket_name)
-bucket_website.put(WebsiteConfiguration=website_configuration)
+    bucket_website = s3_resource.BucketWebsite(bucket_name)
+    bucket_website.put(WebsiteConfiguration=website_configuration)
 
-website_url = f"http://{bucket_name}.s3-website-us-east-1.amazonaws.com"
+    website_url = f"http://{bucket_name}.s3-website-us-east-1.amazonaws.com"
 
-print("S3 website configured")
-print(f"\nEC2 Website: http://{instance.public_ip_address}")
-print(f"S3 Website:  {website_url}")
+    print("S3 website configured")
+    print(f"\nEC2 Website: http://{instance.public_ip_address}")
+    print(f"S3 Website:  {website_url}")
+except Exception as error:
+    print(error)
+    exit(1)
 
 #adding the url to an file
-filename = "jvsilva-websites.txt"
-with open(filename, 'w') as file:
-    file.write(f"EC2 Website: http://{instance.public_ip_address}\n")
-    file.write(f"S3 Website: {website_url}\n")
-print(f"\nURLs written to {filename}")
+try:
+    filename = "jvsilva-websites.txt"
+    with open(filename, 'w') as file:
+        file.write(f"EC2 Website: http://{instance.public_ip_address}\n")
+        file.write(f"S3 Website: {website_url}\n")
+    print(f"\nURLs written to {filename}")
+except Exception as error:
+    print(error)
+    exit(1)
 
 #usign the scp monitoring 
 ip_address = instance.public_ip_address
